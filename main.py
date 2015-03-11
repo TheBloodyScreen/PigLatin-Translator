61cd7b39c00586320849aaecc5be1c316a96fbae
JayDee <TheBloodyScreen@gmail.com>
2015-03-11 16:41:03 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 1 and original.isalpha():
+
+    word = original.lower()
+    first = word[0]
+    new_word = word[1:len(new_word)] + first + pyg
+    print(new_word)
+
+else:
+
+    print 'Word is empty or invalid!'
